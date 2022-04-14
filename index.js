09835ccb7f0844970a574b530a3fa193bd27a930
superfishal <97133716+superfishal@users.noreply.github.com>
2022-04-14 12:27:33 -0700

    add intern, manager, engineer completed

--- index.js
+++ index.js
@@ -1,3 +1,148 @@
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
+const Engineer = require("./lib/Engineer.js");
+const Manager = require("./lib/Manager.js");
+const Intern = require("./lib/Intern.js");
+const employeeArray = [];
+console.log("Welcome to my Profile Generator");
+
+function generateQuestions() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Which type of Employee would you like to add?",
+        choices: ["Manager", "Engineer", "Intern", "Quit"],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.role) {
+        case "Manager":
+          addManager();
+          break;
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+        case "Quit":
+          console.log(employeeArray);
+          break;
+        default:
+          console.log(employeeArray);
+      }
+    });
+}
+function addManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the new Managers name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the new Managers email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the new Managers Office Number?",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      employeeArray.push(manager);
+
+      generateQuestions();
+    });
+}
+
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the new Engineers name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the new Engineers email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the new Engineers github?",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      employeeArray.push(engineer);
+      generateQuestions();
+    });
+}
+
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the new Interns name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the new Interns email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school is the intern attending?",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      employeeArray.push(intern);
+      generateQuestions();
+    });
+}
+
+generateQuestions();

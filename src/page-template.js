64370f1ae27154b59959f51b095bb64a13d59ab7
superfishal <97133716+superfishal@users.noreply.github.com>
2022-04-21 16:11:03 -0700

    icons and colors

--- src/page-template.js
+++ src/page-template.js
@@ -7,7 +7,7 @@ const htmlPageElements = (employeeArray) => {
     <div class="card">
     <div class="card-header">
         <h2 class="card-title">${manager.getName()}</h2>
-        <h3 class="card-title">${manager.getRole()}</h3>
+        <h3 class="card-subtitle mb-2"><i class="stack" style="font-size: 2rem; color: cornflowerblue;"></i>${manager.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
@@ -24,7 +24,7 @@ const htmlPageElements = (employeeArray) => {
     <div class="card">
     <div class="card-header">
         <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title">${engineer.getRole()}</h3>
+        <h3 class="card-subtitle mb-2"><i class="tools" style="font-size: 2rem; color: cornflowerblue;"></i>${engineer.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
@@ -41,7 +41,7 @@ const htmlPageElements = (employeeArray) => {
     <div class="card">
     <div class="card-header">
         <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title">${intern.getRole()}</h3>
+        <h3 class="card-subtitle mb-2"><i class="book-half" style="font-size: 2rem; color: cornflowerblue;"></i>${intern.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
@@ -86,19 +86,20 @@ module.exports = (employeeArray) => {
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       <title>Generated Team Profile</title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
       <link rel="stylesheet" href="style.css">
   </head>
   <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3">
-                <h1 class="text-center">My Team</h1>
+            <div class="col-12 jumbotron mb-3 bg-dark">
+                <h1 class="text-center text-light">My Generated Team</h1>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-12 d-flex justify-content-center">
+            <div class="col-12 d-flex flex-wrap justify-content-center">
                 ${htmlPageElements(employeeArray)}
             </div>
         </div>
